c638bbc2cdfbd23229de80360cd1c470b57c8a17
login <mail@yourmail.ru>
2020-09-24 00:22:42 +0300

    init

---
+++ server/lightning-stadium/src/main/java/com/diploma/lightningstadium/LightningStadiumApplication.java
@@ -0,0 +1,13 @@
+package com.diploma.lightningstadium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightningStadiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightningStadiumApplication.class, args);
+	}
+
+}
---
+++ server/lightning-stadium/src/test/java/com/diploma/lightningstadium/LightningStadiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diploma.lightningstadium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightningStadiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
